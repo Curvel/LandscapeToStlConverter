4f1688f04dd3736e7ec085a26cbcf26650c82129
Pascal Stech <pascal@stech.at>
2019-07-30 10:50:11 +0200

    wip first triangle als byte

--- backend/StlGenerator.go
+++ backend/StlGenerator.go
@@ -17,7 +17,34 @@ func main() {
 
 	n := dif1.Cross(dif2)
 
-	slt := "solid landscape\n" +
+	stl := []byte("solid landscape\n" +
+		"facet normal %f, %f, %f\n" +
+		"outer loop\n" +
+		"vertex 0, 0, 0\n" +
+		"vertex 3, 0, 1\n" +
+		"vertex 0, 3, 2\n" +
+		"endloop\n" +
+		"endfacet\n" +
+		"endsolid landscape")
+
+	stl = fmt.Sprintf(stl, n.X, n.Y, n.Z)
+
+	writeByteToFile(stl)
+
+	log.Print("Test")
+}
+
+func stlAsci() {
+	v1 := r3.Vector{0, 0, 0}
+	v2 := r3.Vector{3, 2, 0}
+	v3 := r3.Vector{0, 1, 3}
+
+	dif1 := v2.Sub(v1)
+	dif2 := v3.Sub(v1)
+
+	n := dif1.Cross(dif2)
+
+	stl := "solid landscape\n" +
 		"facet normal %f, %f, %f\n" +
 		"outer loop\n" +
 		"vertex 0, 0, 0\n" +
@@ -27,21 +54,40 @@ func main() {
 		"endfacet\n" +
 		"endsolid landscape"
 
-	slt = fmt.Sprintf(slt, n.X, n.Y, n.Z)
+	stl = fmt.Sprintf(stl, n.X, n.Y, n.Z)
 
-	writeToFile(slt)
+	writeStringToFile(stl)
 
 	log.Print("Test")
-
 }
 
-func writeToFile(s string) {
+func writeStringToFile(s string) {
 	f, err := os.Create("test.stl")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	l, err := f.WriteString(s)
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	fmt.Println(l, "bytes written successfully")
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func writeByteToFile(b []byte) {
+	f, err := os.Create("test.stl")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	l, err := f.Write(b)
 	if err != nil {
 		fmt.Println(err)
 		f.Close()
